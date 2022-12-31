3fe0c33fda3a2089a116a56deff1c1b84b28c738
Ivan Korotenko <110201295+Vandey89@users.noreply.github.com>
2022-12-31 12:32:21 +0100

    add+1

--- src/containers/header/index.jsx
+++ src/containers/header/index.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Wrapper, Flex, Button } from "components/ui";
 import { HeaderSC, Absolute, HeaderText } from "./styled";
 import { ReactComponent as Logo } from "assets/logo.svg";
+import { LinkContainer } from "containers/footer/styled";
 
 function Header({handleProductScroll}){
 return(
     <HeaderSC>
+        <a href="https://twitter.com/l97496422" rel='noreferrer' target='_blank'></a>
         <Wrapper reLative height="100%">
             <Flex padding="24px 0 0">
                 <Logo />
